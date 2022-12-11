a68e498067f9aa07b39bc54b3670d0ff8cdc7c1e
cherryday <tcarevmaxim@gmail.com>
2022-12-11 16:59:30 +0300

    set layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
+import { Sidebar } from './components/Sidebar/Sidebar'
+import { FolderCreateModal } from './components/FolderCreateModal/FolderCreateModal'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar/>
+      <main className="main">
+        <FolderCreateModal/>
+      </main>
     </div>
   );
 }
