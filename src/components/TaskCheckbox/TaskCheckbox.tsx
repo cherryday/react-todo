94abc9d6a5c3dfce28c3dfa7f4a8f23e6ab32d47
cherryday <tcarevmaxim@gmail.com>
2022-12-14 18:27:55 +0300

    task checkbox

--- src/components/TaskCheckbox/TaskCheckbox.tsx
+++ src/components/TaskCheckbox/TaskCheckbox.tsx
@@ -2,15 +2,28 @@ import { ReactComponent as IconCheck } from '../../assets/icons/check.svg'
 import { ReactComponent as IconCross } from '../../assets/icons/cross.svg'
 import styles from './TaskCheckbox.module.css'
 
-export const TaskCheckbox = (): JSX.Element => {
+interface TaskCheckboxProps {
+  label: string
+  checked: boolean
+  onChange: () => void
+}
+
+export const TaskCheckbox = ({ label, checked, onChange }: TaskCheckboxProps): JSX.Element => {
   return (
     <label className={styles.label}>
-      <input type="checkbox" className={styles.input}/>
+      <input
+        type="checkbox"
+        className={styles.input}
+        checked={checked}
+        onChange={onChange}
+      />
       <span className={styles.checkbox}>
         <IconCheck/>
       </span>
-      Изучить JavaScript
-      <IconCross/>
+      {label}
+      <span className={styles.button}>
+        <IconCross/>
+      </span>
     </label>
   )
 }
