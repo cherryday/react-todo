c7712fd380d47bd0b6b8cd1244ffa08be6bd2a84
cherryday <tcarevmaxim@gmail.com>
2022-12-18 21:26:05 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import { Sidebar } from './components/Sidebar/Sidebar'
 import { HomePage } from './pages/HomePage/HomePage'
 import { FolderPage } from './pages/FolderPage/FolderPage'
+import { TodoProvider } from './context/todo.context'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar/>
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/:folderId" element={<FolderPage/>}/>
-        </Routes>
-      </main>
+      <TodoProvider>
+        <Sidebar/>
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/:folderId" element={<FolderPage/>}/>
+          </Routes>
+        </main>
+      </TodoProvider>
     </div>
   );
 }
