69bf4dd77e031ea485a802b61871ac16a2de209a
cherryday <tcarevmaxim@gmail.com>
2022-12-18 21:21:33 +0300

    home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,21 @@
+import cn from 'classnames'
+import { FolderSection } from '../../components/FolderSection/FolderSection'
+import { useTodoContext } from '../../context/todo.context';
+import styles from './HomePage.module.css'
+
 export const HomePage = (): JSX.Element => {
+  const { folders } = useTodoContext()
+
   return (
-    <h1>Home page</h1>
+    <div className={cn({
+      [styles.empty]: !folders.length
+    })}>
+      {!!folders.length ? folders.map(folder => (
+        <FolderSection
+          key={folder.id}
+          folder={folder}
+        />
+      )) : <h1 className={styles.title}>Задачи отсутствуют</h1>}
+    </div>
   )
 }
