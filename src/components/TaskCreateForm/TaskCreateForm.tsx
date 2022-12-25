a24e1368e92d57f2b3ae0744a8d838f3afa017c2
cherryday <tcarevmaxim@gmail.com>
2022-12-25 16:23:31 +0300

    form event

--- src/components/TaskCreateForm/TaskCreateForm.tsx
+++ src/components/TaskCreateForm/TaskCreateForm.tsx
@@ -2,7 +2,7 @@ import { Input } from '../Input/Input'
 import { Button } from '../Button/Button'
 import styles from './TaskCreateForm.module.css'
 import { useTodoContext } from '../../context/todo.context'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 interface TaskCreateFormProps {
   folderId: string
@@ -13,21 +13,22 @@ export const TaskCreateForm = ({ folderId, cancel }: TaskCreateFormProps): JSX.E
   const [name, setName] = useState<string>('')
   const { createTask } = useTodoContext()
 
-  const handleCreate = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
     createTask(folderId, { name })
     setName('')
   }
 
   return (
-    <div className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <Input
         placeholder="Текст задачи"
         className={styles.input}
         value={name}
         onChange={(event) => setName(event.target.value)}
       />
-      <Button className={styles.create} onClick={handleCreate}>Добавить задачу</Button>
+      <Button className={styles.create}>Добавить задачу</Button>
       <Button color="secondary" onClick={cancel}>Отмена</Button>
-    </div>
+    </form>
   )
 }
