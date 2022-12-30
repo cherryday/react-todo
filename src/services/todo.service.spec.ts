7d91e3db81e5a4eff99e73d55927b85bd2f74397
cherryday <tcarevmaxim@gmail.com>
2022-12-30 23:42:50 +0300

    task service test

--- src/services/todo.service.spec.ts
+++ src/services/todo.service.spec.ts
@@ -1,8 +1,14 @@
-import { getFolders } from './todo.service'
+import { v4 } from 'uuid'
+import { getFolders, createFolder, FOLDER_COLORS } from './todo.service'
+
+jest.mock('uuid', () => ({
+  v4: jest.fn(),
+}))
 
 Object.defineProperty(window, 'localStorage', {
   value: {
-    getItem: jest.fn()
+    getItem: jest.fn(),
+    setItem: jest.fn(),
   }
 })
 
@@ -10,5 +16,14 @@ describe('todo.service', () => {
   it('getFolders', () => {
     getFolders()
     expect(localStorage.getItem).toHaveBeenCalledWith('todo')
+  })
+
+  it('createFolder', () => {
+    const uuidv4 = v4 as jest.Mock
+    uuidv4.mockReturnValue('id')
+    const payload = { name: 'name', color: FOLDER_COLORS[0] }
+    createFolder(payload)
+    console.log(v4);
+    expect(localStorage.setItem).toHaveBeenCalledWith('todo', JSON.stringify([{ id: 'id', tasks: [], ...payload }]))
   })
 })
