c073c5909d462f29baa3c4b71774ce3132344e93
cherryday <tcarevmaxim@gmail.com>
2023-01-04 12:24:03 +0300

    test

--- src/components/FolderSection/FolderSection.spec.tsx
+++ src/components/FolderSection/FolderSection.spec.tsx
@@ -1,18 +1,21 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Folder, FOLDER_COLORS } from '../../services/todo.service'
+import { createMockFolder, createMockTask } from '../../test-helpers'
 import { TodoProvider } from '../../context/todo.context'
 import { FolderSection } from './FolderSection'
 
 describe('FolderSection', () => {
-  it('should open and close form', () => {
-    const folder: Folder = {
-      id: '',
-      name: '',
-      color: FOLDER_COLORS[0],
-      tasks: [],
-    }
+  it('should render content', () => {
+    const folder = createMockFolder()
+    folder.tasks = [createMockTask(), createMockTask()]
     render(<FolderSection folder={folder}/>, { wrapper: TodoProvider })
+    
+    expect(screen.getByRole('heading', { name: folder.name })).toBeInTheDocument()
+    expect(screen.getAllByRole('checkbox')).toHaveLength(2)
+  })
+
+  it('should open and close form', () => {
+    render(<FolderSection folder={createMockFolder()}/>, { wrapper: TodoProvider })
     expect(screen.queryByRole('form')).not.toBeInTheDocument()
 
     userEvent.click(screen.getByRole('button', { name: /новая задача/i }))
